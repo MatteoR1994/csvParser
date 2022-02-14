92ae23952d24c1ac6d83c443dd8cc8c13d42e220
MatteoR1994 <matteo.rinaldi1893@gmail.com>
2022-02-14 11:03:58 +0100

    Create hangman.js

---
+++ hangman.js
@@ -0,0 +1,16 @@
+class Hangman {
+
+    constructor() {
+        this.actualScore = 0;
+        this.record = 0;
+        this.extractWord = "";
+        this.extractWordForUser = "";
+        this.wordsAlreadyExtracted = [];
+        this.lives = 10;
+    }
+
+    checkCharPresence() {
+        
+    }
+
+}
